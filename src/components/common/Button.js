0eb0e8897fb7128f64087d9f637b3b7091c9de37
FrankDannie <frank.dannie@gmail.com>
2024-07-31 15:12:34 +0900

    feat: Add common features and components

---
+++ src/components/common/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css'; 
+
+const Button = ({ onClick, children, className }) => (
+  <button className={`button ${className}`} onClick={onClick}>
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  className: '',
+};
+
+export default Button;
