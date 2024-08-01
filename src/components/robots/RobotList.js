809d97aa69bbef3cee9193e2307c0dae3c95dfb4
FrankDannie <frank.dannie@gmail.com>
2024-08-01 12:52:28 +0900

    UI: Change in UI of RobotList

--- src/components/robots/RobotList.js
+++ src/components/robots/RobotList.js
@@ -8,14 +8,12 @@ const RobotList = ({ robots, onSelect }) => {
         <thead>
           <tr>
             <th>Name</th>
-            <th>Model Name</th>
           </tr>
         </thead>
         <tbody>
           {robots.map((robot) => (
             <tr key={robot.id} onClick={() => onSelect(robot.id)}>
               <td>{robot.name}</td>
-              <td>{robot.model_name}</td>
             </tr>
           ))}
         </tbody>
